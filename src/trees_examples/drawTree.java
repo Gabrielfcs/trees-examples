48928cf3adf3d049cd7d58b1626d0b6ccececd57
Gabriel Felipe <gabriel_fcs@outlook.com>
2019-05-26 01:53:50 -0300

    Remove comments and re-indent lines

--- src/trees_examples/drawTree.java
+++ src/trees_examples/drawTree.java
@@ -13,44 +13,31 @@ import javax.swing.JPanel;
 
 class DrawTree extends JPanel{
 	
-	public Tree tree;
-	
-	public DrawTree(Tree tree){
-            this.tree = tree;
-	}
-	
-	@Override
-	protected void paintComponent(Graphics g) {
-            // TODO Auto-generated method stub
+    public Tree tree;
 
-            g.setFont(new Font("Tahoma", Font.BOLD, 20));
-            //g.drawString(String.valueOf(tree.root.data), this.getWidth()/2, 30);
+    public DrawTree(Tree tree){
+        this.tree = tree;
+    }
 
+    @Override
+    protected void paintComponent(Graphics g) {
+        g.setFont(new Font("Tahoma", Font.BOLD, 20));
+        DrawTree(g, 0, getWidth(), 0, getHeight() / tree.getDepth(tree.getRoot()), tree.getRoot());
+    }
 
-                    //DrawNode(g, tree.root,100, 50,2);
+    public void DrawNode(Graphics g,Node n,int w,int h,int q){
+        g.setFont(new Font("Tahoma", Font.BOLD, 20));
 
-            DrawTree(g, 0, getWidth(), 0, getHeight() / tree.getDepth(tree.getRoot()), tree.getRoot());
-	}
-	
-	public void DrawNode(Graphics g,Node n,int w,int h,int q){
-		g.setFont(new Font("Tahoma", Font.BOLD, 20));
-		
-		if(n!=null){
-			
-			g.drawString(String.valueOf(n.getValue()), (this.getWidth()/q)+w, h);
-			if(n.getLeft() !=null)
-				DrawNode(g, n.getLeft(), -w, h*2, q);
-				//DrawNode(g, n.left, -w, h*2, q);
-				//g.drawString(String.valueOf(n.left.data), (this.getWidth()/q)-w, h+50);
-			if(n.getRight() !=null)
-				DrawNode(g, n.getRight(), w*2, h*2, q);
-			//g.drawString(String.valueOf(n.right.data), (this.getWidth()/q)+w, h+50);
-		}
-		
-		
-		
-		
-	}
+        if(n!=null){
+            g.drawString(String.valueOf(n.getValue()), (this.getWidth()/q)+w, h);
+            if(n.getLeft() !=null) {
+                DrawNode(g, n.getLeft(), -w, h*2, q);
+            }
+            if(n.getRight() !=null) {
+                DrawNode(g, n.getRight(), w*2, h*2, q);
+            }
+        }
+    }
 	
 	
     public void DrawTree(Graphics g, int StartWidth, int EndWidth, int StartHeight, int Level, Node node) {
@@ -60,12 +47,12 @@ class DrawTree extends JPanel{
         int dataWidth = fm.stringWidth(data);
         g.drawString(data, (StartWidth + EndWidth) / 2 - dataWidth / 2, StartHeight + Level / 2);
 
-        if (node.getLeft() != null)            
-        	DrawTree(g, StartWidth, (StartWidth + EndWidth) / 2, StartHeight + Level, Level, node.getLeft());
+        if (node.getLeft() != null) {
+            DrawTree(g, StartWidth, (StartWidth + EndWidth) / 2, StartHeight + Level, Level, node.getLeft());
+        }
         
-        if (node.getRight() != null)
-        	DrawTree(g, (StartWidth + EndWidth) / 2, EndWidth, StartHeight + Level, Level, node.getRight());
-    }
-	
-	
+        if (node.getRight() != null) {
+            DrawTree(g, (StartWidth + EndWidth) / 2, EndWidth, StartHeight + Level, Level, node.getRight());
+        }
+    }	
 }
--- src/trees_examples/treeGUI.java
+++ src/trees_examples/treeGUI.java
@@ -13,25 +13,24 @@ import javax.swing.border.EmptyBorder;
  */
 public class treeGUI extends JFrame {
 
-	private JPanel contentPane;
-	public Tree tree;
-	public DrawTree drawer;
-	
-	/**
-	 * Create the frame.
-	 */
-	public treeGUI(Tree tree) {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 500, 500);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		drawer = new DrawTree(tree);
-		
-		contentPane.add(drawer);
-		setContentPane(contentPane);
-		this.tree = tree;
-		setVisible(true);
-	}
+    private JPanel contentPane;
+    public Tree tree;
+    public DrawTree drawer;
 
+    /**
+     * Create the frame.
+     */
+    public treeGUI(Tree tree) {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 500, 500);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.setLayout(new BorderLayout(0, 0));
+        drawer = new DrawTree(tree);
+
+        contentPane.add(drawer);
+        setContentPane(contentPane);
+        this.tree = tree;
+        setVisible(true);
+    }
 }
